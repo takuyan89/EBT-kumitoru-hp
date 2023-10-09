c39d623e8491b487b98a08c9ce5b3ad8b9ed4f79
Tsui <62528268+ngby@users.noreply.github.com>
2023-10-09 13:10:03 +0900

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { Kv } from '@/component/kv'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-4xl font-bold text-center">Hello World</h1>
+    <main className="">
+			<Kv />
     </main>
   )
 }
---
+++ src/component/kv.jsx
@@ -0,0 +1,7 @@
+export const Kv = () => {
+	return (
+		<div className="">
+			<p>制作費０円！サブスクで目的に最適化されたホームページを制作します。</p>
+		</div>
+	);
+}
